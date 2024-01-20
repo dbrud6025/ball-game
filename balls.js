cae91621a5d80743c590b7d79257e7bd44eb7fa8
dbrud6025 <mapu6025@gmail.com>
2024-01-20 18:46:01 +0900

    first commit

---
+++ balls.js
@@ -0,0 +1,34 @@
+export const BALLS = [
+    {
+        name: "01_pingpong",
+        radius: 61/2
+    },
+    {
+        name: "02_tennis",
+        radius: 80/2
+    },
+    {
+        name: "03_base",
+        radius: 95/2
+    },
+    {
+        name: "04_rugby",
+        radius: 114/2
+    },
+    {
+        name: "05_volley",
+        radius: 140/2
+    },
+    {
+        name: "06_basket",
+        radius: 180/2
+    },
+    {
+        name: "07_bowling",
+        radius: 200/2
+    },
+    {
+        name: "08_gold_soccer",
+        radius: 240/2
+    }
+];
